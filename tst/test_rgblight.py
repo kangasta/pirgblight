a29d363acd24d50fcae7e18a3bdaa21107a66b24
Toni Kangas <toni.ar.kangas@gmail.com>
2019-10-07 02:37:41 +0300

    Add test for RGB to HSV conversions provided by the base class

--- tst/test_rgblight.py
+++ tst/test_rgblight.py
@@ -3,6 +3,18 @@ from unittest.mock import MagicMock, patch
 
 from pirgblight import RGBLight
 
+class DummyRGBLight(RGBLight):
+    def __init__(self):
+        self._rgb = (0,0,0,)
+
+    @property
+    def color(self):
+        return self._rgb
+
+    @color.setter
+    def color(self, rgb):
+        self._rgb = rgb
+
 class RgbLightTest(TestCase):
     def test_raises_not_implemented_error(self):
         led = RGBLight()
@@ -12,3 +24,14 @@ class RgbLightTest(TestCase):
 
         with self.assertRaises(NotImplementedError):
             led.color = (4, 5, 6,)
+
+    def test_supports_hsv_values(self):
+        led = DummyRGBLight()
+        black = (0,0,0,)
+        white = (255,255,255,)
+
+        led.color = black
+        self.assertTupleEqual(led.hsv_color, black)
+
+        led.hsv_color = (0,0,1)
+        self.assertTupleEqual(led.color, white)
